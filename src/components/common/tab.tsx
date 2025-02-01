4155ee5932f3c036310ece9507acb4bd8d149de9
Hemabh <hemabh@foxsense.io>
2025-02-02 03:15:28 +0530

    updated tab fonts

--- src/components/common/tab.tsx
+++ src/components/common/tab.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useMemo } from "react";
+import { FunctionComponent, useMemo, useState } from "react";
 import { colors } from "../../constants/colors";
 import { Tab, tabStore } from "../../stores/tab-store";
 import { observer } from "mobx-react-lite";
@@ -12,6 +12,7 @@ interface TabProps {
 
 const TabButton: FunctionComponent<TabProps> = ({ tabData }: TabProps) => {
   const navigate = useNavigate();
+  const [isHovering, setIsHovering] = useState(false);
 
   const isSelected = tabStore.currentTab === tabData.tab;
 
@@ -26,16 +27,18 @@ const TabButton: FunctionComponent<TabProps> = ({ tabData }: TabProps) => {
         alignItems: "center",
         backgroundColor: colors.darkerLightGreen,
         color: isSelected ? colors.darkRed : colors.darkTeal,
-        fontWeight: isSelected ? "bold" : "normal",
-        fontFamily: isSelected ? fonts.danielSans : fonts.alteHaasGrotesk,
+        fontWeight: isSelected || isHovering ? "normal" : "bold",
+        fontFamily: isSelected || isHovering ? fonts.danielSans : fonts.alteHaasGrotesk,
         borderBottom: isSelected ? `2px solid ${colors.darkRed}` : "none",
+        fontSize: isSelected || isHovering ? "22px" : "18px",
         transition: "color 0.2s ease, border-bottom 0.2s ease, font-weight 100ms ease-in-out, font-family 100ms ease-in-out, font-size 100ms ease-in-out, height 100ms ease-in-out"
-
       }}
       onClick={() => {
         tabStore.setCurrentTab(tabData.tab);
           navigate(tabData.route.path)
-      } }
+      }}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
     >
       {tabData.label}
     </div>);
