7fc808e2c964f55f906fd576b7f25287d041f30c
Hemabh <hemabh@foxsense.io>
2025-02-10 02:23:12 +0530

    responsiveness update 2

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,29 +1,45 @@
 import { FunctionComponent } from "react";
+import styled from "styled-components";
 import { colors } from "../constants/colors";
 
 interface LayoutProps {
   children?: React.ReactNode;
-  style?: React.CSSProperties
-
+  style?: React.CSSProperties;
 }
 
-const MainCard: FunctionComponent<LayoutProps> = ({children, style}: LayoutProps) => {
+const StyledDiv = styled.div`
+  background-color: ${colors.lightGreen};
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  padding: 100px 15%;
+  border-radius: 20px;
+  color: ${colors.darkTeal};
+  ${props => props.style && Object.entries(props.style).map(([key, value]) => `${key}: ${value};`).join('\n')}
+
+
+  @media (min-width: 640px) {
+  }
+
+  @media (min-width: 768px) {
+  }
+
+  @media (min-width: 1024px) {
+    gap: 30px;
+    padding: 40px 15%;
+  }
+
+  @media (min-width: 1280px) {
+
+  }
+`;
+
+const MainCard: FunctionComponent<LayoutProps> = ({ children, style }: LayoutProps) => {
   return (
-    <div
-      style={{
-        backgroundColor: colors.lightGreen,
-        display: "Flex",
-        flexDirection: "column",
-        gap: "60px",
-        padding: "100px 15%",
-        borderRadius: "20px",
-        color: colors.darkTeal,
-        // height: "80vh",
-        ...style  // Fixed the spread operator syntax
-    }}>
-        {children}
-    </div>
+    <StyledDiv style={style}>
+      {children}
+    </StyledDiv>
   );
-}
+};
 
 export default MainCard;
